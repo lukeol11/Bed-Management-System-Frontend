ebb0dbeca351e5cd3f14100a1e96574d42755ddb
Luke O'Leary <luke.oleary1@gmail.com>
2024-03-23 17:02:46 +0000

    test: updated test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,14 +1,44 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
 import TopBar from "@/components/Layout/TopBar.vue";
+import Vuex from "vuex";
+import CarbonComponentsVue from "@carbon/vue";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+localVue.use(CarbonComponentsVue);
 
 describe("TopBar.vue", () => {
-    it("renders props.msg when passed", () => {
-        const wrapper = shallowMount(TopBar, {
-            propsData: {
-                selectedHospital: "test",
-                hospitalList: ["Test", "Test2"]
+    let store;
+
+    beforeEach(() => {
+        store = new Vuex.Store({
+            state: {},
+            getters: {
+                allHospitals: jest.fn(() => {
+                    return [
+                        {
+                            id: 1,
+                            description: "Hospital 1"
+                        }
+                    ];
+                }),
+                getUserDetails: jest.fn(() => {
+                    return {
+                        hospital_id: 1
+                    };
+                })
+            },
+            mutations: {
+                SET_SELECTED_HOSPITAL: jest.fn()
             }
         });
-        expect(wrapper.text()).toContain("test");
+    });
+
+    it("renders basic html topbar", () => {
+        const wrapper = shallowMount(TopBar, {
+            localVue,
+            store
+        });
+        expect(wrapper.html()).toContain("cv-select");
     });
 });
