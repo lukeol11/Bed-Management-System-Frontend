24d14bafadc8fac261a0186526f0f3a42ee92e5c
Luke O'Leary <luke.oleary1@gmail.com>
2024-04-07 15:19:01 +0100

    fix: sonar maintainability issue

--- src/components/Layout/MainLayout.vue
+++ src/components/Layout/MainLayout.vue
@@ -40,8 +40,9 @@ export default {
 @media (max-width: 768px) {
     div.Layout div.content {
         margin-left: 0.2rem;
+        margin-right: 0.2rem;
         margin-top: 0.2rem;
-        margin: 0.2rem;
+        margin-bottom: 0.2rem;
     }
     nav.sideBar,
     header.topBar {
