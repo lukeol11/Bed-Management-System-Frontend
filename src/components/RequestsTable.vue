2fb202d0b87a4058ea86f958e6217315e36b111a
Luke O'Leary <luke.oleary1@gmail.com>
2024-04-08 19:49:55 +0100

    feat: Added additional columns

--- src/components/RequestsTable.vue
+++ src/components/RequestsTable.vue
@@ -12,10 +12,19 @@
                 >
                     <cv-data-table-cell>{{ result.id }}</cv-data-table-cell>
                     <cv-data-table-cell>{{
+                        result.patientName
+                    }}</cv-data-table-cell>
+                    <cv-data-table-cell>{{
                         result.hospital
                     }}</cv-data-table-cell>
                     <cv-data-table-cell>{{
+                        result.currentWard
+                    }}</cv-data-table-cell>
+                    <cv-data-table-cell>{{
                         result.currentBed
+                    }}</cv-data-table-cell>
+                    <cv-data-table-cell>{{
+                        result.requestedWard
                     }}</cv-data-table-cell>
                     <cv-data-table-cell>{{
                         result.requestedBed
@@ -46,8 +55,11 @@ export default {
         return {
             columns: [
                 "ID",
+                "Patient Name",
                 "Current Hospital",
+                "Current Ward",
                 "Current Bed",
+                "Requested Ward",
                 "Requested Bed",
                 "Request by",
                 "Request Made At",
@@ -56,16 +68,22 @@ export default {
             results: [
                 {
                     id: 1,
-                    hospital: "Hospital 1",
+                    patientName: "John Doe",
+                    hospital: "University Hospital",
+                    currentWard: "CDU",
                     currentBed: "Bed 1",
+                    requestedWard: "CDU",
                     requestedBed: "Bed 2",
                     requestBy: "Doctor 1",
                     requestTime: "2021-01-01 12:00:00"
                 },
                 {
                     id: 2,
-                    hospital: "Hospital 2",
+                    patientName: "Jane Doe",
+                    hospital: "University Maternity Hospital",
+                    currentWard: "Maternity",
                     currentBed: "Bed 2",
+                    requestedWard: "POCU",
                     requestedBed: "Bed 3",
                     requestBy: "Doctor 2",
                     requestTime: "2021-01-01 12:00:00"
@@ -83,3 +101,9 @@ export default {
     watch: {}
 };
 </script>
+
+<style scoped>
+.bx--btn-set .bx--btn {
+    width: 50%;
+}
+</style>
