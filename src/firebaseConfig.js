d10c1a224f8bb615681e3deed10b9ede20485355
Luke O'Leary <luke.oleary1@gmail.com>
2024-06-11 15:56:15 +0100

    feat: Exposed firebase config to git

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDu8LogrYRdoUOIPIvrHdqV5jk_ZzpZvVY",
+    authDomain: "bed-management-system-fyp.firebaseapp.com",
+    projectId: "bed-management-system-fyp",
+    storageBucket: "bed-management-system-fyp.appspot.com",
+    messagingSenderId: "40400236",
+    appId: "1:40400236:web:4509cae0f1fe8db9112f49",
+    measurementId: "G-9LMXZ39HYZ"
+};
